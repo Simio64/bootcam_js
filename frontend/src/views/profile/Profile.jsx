48bc6525c0d2e9c17abe6b490fb05ff4fa8b4023
Alberto Arias <alberto.arias.x64@gmail.com>
2024-04-29 07:39:20 -0500

    profile update

--- frontend/src/views/profile/Profile.jsx
+++ frontend/src/views/profile/Profile.jsx
@@ -26,23 +26,20 @@ const Profile = () => {
     socket.on('@server/new_message', () => {
       userNotes(id).then(data => set_notes(data))
     })
-  }, [socket]);
+  }, [id]);
 
   return (
     <Principal>
-      <section id='nepe'>
-        <div id='profile'>
-          <div id='profile_contend'>
-            <img src={`${urlBase}/${user.imagen}`} alt="user_img" />
-            <div id='contend'>
-              <h1 id='name'>{user.name}</h1>
-              <p>ID: {id}</p>
-              <h2 id='mail'>{user.mail}</h2>
-            </div>
+      <section className="app-profile">
+        <div className='app-profile-contend'>
+          <img src={`${urlBase}/${user.imagen}`} alt="user_img" />
+          <div className='app-profile-data'>
+            <h1>{user.name}</h1>
+            <h3>{user.mail}</h3>
+            <p>ID: {id}</p>
           </div>
-          <div id='decorador'></div>
-          <Notes notes={notes} charge={false} delete_button={id === localStorage.getItem('id_user') ? true : false} />
         </div>
+        <Notes notes={notes} charge={false} delete_button={id === localStorage.getItem('id_user') ? true : false} />
       </section>
     </Principal>
   )
